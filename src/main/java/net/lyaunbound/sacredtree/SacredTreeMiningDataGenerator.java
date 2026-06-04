691e1f00540bd18caf6c792c346a9c7be4fca630
LyaUnbound <soul22eternal@protonmail.com>
2026-06-04 08:51:32 -0600

    Initial commit

---
+++ src/main/java/net/lyaunbound/sacredtree/SacredTreeMiningDataGenerator.java
@@ -0,0 +1,11 @@
+package net.lyaunbound.sacredtree;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SacredTreeMiningDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
